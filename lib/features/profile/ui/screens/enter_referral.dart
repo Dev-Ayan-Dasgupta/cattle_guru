44a319d8f79d6c4f085409a1239df04f857f755c
Ayan Dasgupta <dev.ayandg@gmail.com>
2022-11-18 12:57:27 +0530

    fixed enter reffereal page

--- lib/features/profile/ui/screens/enter_referral.dart
+++ lib/features/profile/ui/screens/enter_referral.dart
@@ -37,161 +37,103 @@ class _EnterReferralScreenState extends State<EnterReferralScreen> {
         child: GestureDetector(
           onTap: (){ FocusManager.instance.primaryFocus?.unfocus();},
           behavior: HitTestBehavior.opaque,
-          child: Column(
-            children: [
-              StreamBuilder(
-                stream: FirebaseFirestore.instance.collection('customers').snapshots(),
-                builder: (context, snapshot) {
-                  return ListView.builder(
-                    itemCount: (snapshot.data != null) ? snapshot.data!.docs.length : 0,
-                    itemBuilder: (context, index){
-                      if(snapshot.hasData){
-                        allUsersId.add(snapshot.data!.docs[index].get('uid'));
-                        if(snapshot.data!.docs[index].get('uid') == currUserId){
-                          currentUserName = snapshot.data!.docs[index].get('name');
-                        }
-
-                        return SizedBox();
-                        
-                        // return SingleChildScrollView(
-                        //   child: Padding(
-                        //     padding: EdgeInsets.symmetric(horizontal: 5.w),
-                        //     child: Column(
-                        //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        //       children: [
-                        //         Column(
-                        //           crossAxisAlignment: CrossAxisAlignment.start,
-                        //           children: [
-                        //             SizedBox(height: 15.h,),
-                        //             Center(
-                        //               child: SizedBox(
-                        //                 width: 40.w,
-                        //                 height: 40.w,
-                        //                 child: const Image(image: AssetImage("./assets/images/referral.png"), fit: BoxFit.fill,),
-                        //               ),
-                        //             ),
-                        //             SizedBox(height: 5.h,),
-                        //             Text(isEnglish ? "Enter Referral Code" : "रेफ़रल कोड दर्ज करें", style: globalTextStyle.copyWith(color: black, fontSize: 4.w, fontWeight: FontWeight.bold),),
-                        //             SizedBox(height: 1.5.h,),
-                        //             CustomTextField(controller: referralCodeController, hintText: "JvpXGE53p9O4X0mhpoREtw3Ar2w2", label: "Referral Code", keyboardType: TextInputType.text, width: 90.w),
-                        //             SizedBox(height: 1.h,),
-                        //             Row(
-                        //               children: [
-                        //                 Text(isEnglish ? "You and your friend will each receive " : "साइन अप करने पर आपको और आपके दोस्त को ", style: globalTextStyle.copyWith(color: black, fontSize: 3.w,),),
-                        //                 Text("₹ 100 ", style: globalTextStyle.copyWith(color: primary, fontSize: 3.w, fontWeight: FontWeight.bold),),
-                        //                 Text(isEnglish ? "when you sign up." : "मिलेंगे।", style: globalTextStyle.copyWith(color: black, fontSize: 3.w,),),
-                        //               ],
-                        //             ),
-                        //           ],
-                        //         ),
-                        //         Column(
-                        //           children: [
-                        //             SizedBox(height: 23.h,),
-                        //             CustomButton(width: 90.w, height: 15.w, color: primary, 
-                        //             onTap: (){
-                        //               if(allUsersId.contains(referralCodeController.text) && (referralCodeController.text != currUserId)){
-                        //                 FirebaseFirestore.instance.collection('customers').doc(currUserId).update({
-                        //                   'walletBalance': FieldValue.increment(100),
-                        //                 });
-                        //                 FirebaseFirestore.instance.collection('customers').doc(referralCodeController.text).update({
-                        //                   'walletBalance': FieldValue.increment(100),
-                        //                 });
-                        //                 FirebaseFirestore.instance.collection('customers').doc(referralCodeController.text).update({
-                        //                   'transactions': FieldValue.arrayUnion([
-                        //                     {
-                        //                       'purpose': "Sign up by $currentUserName",
-                        //                       'date': DateTime.now(),
-                        //                       'amount': 100,
-                        //                     }
-                        //                   ]),
-                        //                 });
-                        //               }
-                                      
-                        //               Navigator.pushNamed(context, home);
-                        //             }, 
-                        //             text: isEnglish ? "Submit" : "दर्ज करें", fontColor: white, borderColor: primary),
-                        //             SizedBox(height: 2.h,)
-                        //           ],
-                        //         ),
-                        //       ],
-                        //     ),
-                        //   ),
-                        // );
-                      }
-                      if (snapshot.hasError) {
-                        return const Text('Error');
-                      } else {
-                        return const SizedBox();
-                      }
-                    },
-                  ); 
-                }
-              ),
-              SingleChildScrollView(
-                child: Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 5.w),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          SizedBox(height: 15.h,),
-                          Center(
-                            child: SizedBox(
-                              width: 40.w,
-                              height: 40.w,
-                              child: const Image(image: AssetImage("./assets/images/referral.png"), fit: BoxFit.fill,),
-                            ),
-                          ),
-                          SizedBox(height: 5.h,),
-                          Text(isEnglish ? "Enter Referral Code" : "रेफ़रल कोड दर्ज करें", style: globalTextStyle.copyWith(color: black, fontSize: 4.w, fontWeight: FontWeight.bold),),
-                          SizedBox(height: 1.5.h,),
-                          CustomTextField(controller: referralCodeController, hintText: "JvpXGE53p9O4X0mhpoREtw3Ar2w2", label: "Referral Code", keyboardType: TextInputType.text, width: 90.w),
-                          SizedBox(height: 1.h,),
-                          Row(
+          child: StreamBuilder(
+            stream: FirebaseFirestore.instance.collection('customers').snapshots(),
+            builder: (context, snapshot) {
+              return ListView.builder(
+                itemCount: (snapshot.data != null) ? snapshot.data!.docs.length : 0,
+                itemBuilder: (context, index){
+                  if(snapshot.hasData){
+                    allUsersId.add(snapshot.data!.docs[index].get('uid'));
+                    if(snapshot.data!.docs[index].get('uid') == currUserId){
+                      currentUserName = snapshot.data!.docs[index].get('name');
+                      return SingleChildScrollView(
+                        child: Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 5.w),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              Text(isEnglish ? "You and your friend will each receive " : "साइन अप करने पर आपको और आपके दोस्त को ", style: globalTextStyle.copyWith(color: black, fontSize: 3.w,),),
-                              Text("₹ 100 ", style: globalTextStyle.copyWith(color: primary, fontSize: 3.w, fontWeight: FontWeight.bold),),
-                              Text(isEnglish ? "when you sign up." : "मिलेंगे।", style: globalTextStyle.copyWith(color: black, fontSize: 3.w,),),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  SizedBox(height: 15.h,),
+                                  Center(
+                                    child: SizedBox(
+                                      width: 40.w,
+                                      height: 40.w,
+                                      child: const Image(image: AssetImage("./assets/images/referral.png"), fit: BoxFit.fill,),
+                                    ),
+                                  ),
+                                  SizedBox(height: 5.h,),
+                                  Text(isEnglish ? "Enter Referral Code" : "रेफ़रल कोड दर्ज करें", style: globalTextStyle.copyWith(color: black, fontSize: 4.w, fontWeight: FontWeight.bold),),
+                                  SizedBox(height: 1.5.h,),
+                                  CustomTextField(controller: referralCodeController, hintText: "JvpXGE53p9O4X0mhpoREtw3Ar2w2", label: "Referral Code", keyboardType: TextInputType.text, width: 90.w),
+                                  SizedBox(height: 1.h,),
+                                  Row(
+                                    children: [
+                                      Text(isEnglish ? "You and your friend will each receive " : "साइन अप करने पर आपको और आपके दोस्त को ", style: globalTextStyle.copyWith(color: black, fontSize: 3.w,),),
+                                      Text("₹ 100 ", style: globalTextStyle.copyWith(color: primary, fontSize: 3.w, fontWeight: FontWeight.bold),),
+                                      Text(isEnglish ? "when you sign up." : "मिलेंगे।", style: globalTextStyle.copyWith(color: black, fontSize: 3.w,),),
+                                    ],
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  SizedBox(height: 23.h,),
+                                  CustomButton(width: 90.w, height: 15.w, color: primary, 
+                                  onTap: (){
+                                    if(allUsersId.contains(referralCodeController.text) && (referralCodeController.text != currUserId)){
+                                      FirebaseFirestore.instance.collection('customers').doc(currUserId).update({
+                                        'walletBalance': FieldValue.increment(100),
+                                      });
+                                      FirebaseFirestore.instance.collection('customers').doc(referralCodeController.text).update({
+                                        'walletBalance': FieldValue.increment(100),
+                                      });
+                                      FirebaseFirestore.instance.collection('customers').doc(referralCodeController.text).update({
+                                        'transactions': FieldValue.arrayUnion([
+                                          {
+                                            'purpose': "Sign up by $currentUserName",
+                                            'date': DateTime.now(),
+                                            'amount': 100,
+                                          }
+                                        ]),
+                                      });
+                                      FirebaseFirestore.instance.collection('customers').doc(currUserId).update({
+                                        'transactions': FieldValue.arrayUnion([
+                                          {
+                                            'purpose': "Self Sign up",
+                                            'date': DateTime.now(),
+                                            'amount': 100,
+                                          }
+                                        ]),
+                                      });
+                                    }
+                                    
+                                    Navigator.pushNamed(context, home);
+                                  }, 
+                                  text: isEnglish ? "Submit" : "दर्ज करें", fontColor: white, borderColor: primary),
+                                  SizedBox(height: 2.h,)
+                                ],
+                              ),
                             ],
                           ),
-                        ],
-                      ),
-                      Column(
-                        children: [
-                          SizedBox(height: 23.h,),
-                          CustomButton(width: 90.w, height: 15.w, color: primary, 
-                          onTap: (){
-                            if(allUsersId.contains(referralCodeController.text) && (referralCodeController.text != currUserId)){
-                              FirebaseFirestore.instance.collection('customers').doc(currUserId).update({
-                                'walletBalance': FieldValue.increment(100),
-                              });
-                              FirebaseFirestore.instance.collection('customers').doc(referralCodeController.text).update({
-                                'walletBalance': FieldValue.increment(100),
-                              });
-                              FirebaseFirestore.instance.collection('customers').doc(referralCodeController.text).update({
-                                'transactions': FieldValue.arrayUnion([
-                                  {
-                                    'purpose': "Sign up by $currentUserName",
-                                    'date': DateTime.now(),
-                                    'amount': 100,
-                                  }
-                                ]),
-                              });
-                            }
-                            
-                            Navigator.pushNamed(context, home);
-                          }, 
-                          text: isEnglish ? "Submit" : "दर्ज करें", fontColor: white, borderColor: primary),
-                          SizedBox(height: 2.h,)
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
+                        ),
+                      );
+                    }
+
+                    // else {
+                    //   return SizedBox();
+                    // }
+                  }
+                  if (snapshot.hasError) {
+                    return const Text('Error');
+                  } else {
+                    return const SizedBox();
+                  }
+                },
+              ); 
+            }
           ),
         )
       )
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,6 +11,7 @@ import firebase_auth
 import firebase_core
 import firebase_storage
 import geolocator_apple
+import package_info_plus_macos
 import path_provider_macos
 import share_plus_macos
 import smart_auth
@@ -23,6 +24,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SmartAuthPlugin.register(with: registry.registrar(forPlugin: "SmartAuthPlugin"))
